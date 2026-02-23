1de77fd44dbf21466da1a902663a5cbac6826829
ABOaseel20 <134551320+ABOaseel20@users.noreply.github.com>
2026-02-23 05:05:19 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,16 +5,15 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+// maintenance
+if (file_exists($maintenance = __DIR__.'/almasah/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+// autoload
+require __DIR__.'/almasah/vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+// bootstrap
+$app = require_once __DIR__.'/almasah/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
